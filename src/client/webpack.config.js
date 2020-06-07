183c1ba98e98b9fd2a0a7a6b71d4e2ae742b38b1
Hayden Lee <haydenlee22@gmail.com>
2020-06-07 13:00:24 -0400

    Added Entry point and added alert script to Index.js

---
+++ src/client/webpack.config.js
@@ -0,0 +1,6 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+entry: './src/client/index.js',  // starting point where webpack will begin to build our tree from
+}
